5736838383ba07416a3a20f42f87ef87103f64fb
Nguyen Cong Duy <ncongduy@gmail.com>
2022-07-15 00:42:59 +0300

    feat: create a service for User model

---
+++ src/user/user.service.js
@@ -0,0 +1,36 @@
+import UserModel from './user.model.js';
+
+const getAll = async () => {
+	const allUser = await UserModel.findAll();
+
+	return allUser;
+};
+
+const getOne = async (id) => {
+	const foundUser = await UserModel.findOne({where: {id: id}});
+
+	if (!foundUser) throw Error('User does not exist.');
+
+	return foundUser;
+};
+
+const create = async (data) => {
+	const newUser = await UserModel.create(data);
+
+	return newUser;
+};
+
+const update = async (data, id) => {
+	await getOne(id);
+	await UserModel.update(data, {where: {id: id}});
+	const newUpdateUser = await getOne(id);
+
+	return newUpdateUser;
+};
+
+const deleteOne = async (id) => {
+	await getOne(id);
+	await UserModel.destroy({where: {id: id}});
+};
+
+export default {getAll, getOne, create, update, deleteOne};
